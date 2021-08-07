0e0086384cd71d89c7d5445d5599f287e08f7431
ferdiws <ferdinand.wijaya@mail.ugm.ac.id>
2021-08-07 20:23:16 +0700

    fix upload

--- middleware/uploadFile.js
+++ middleware/uploadFile.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/file/')
+        cb(null, 'uploads/')
     },
     filename: (req, file, cb) => {
         let ext = path.extname(file.originalname)
